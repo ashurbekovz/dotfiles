2593e605eaa6e4d10e030cb8374b637df9c2cb42
Zamir Ashurbekov <derbent_21@outlook.com>
2024-11-12 23:08:59 +0300

    feat: add telescope

--- lua/plugins/plugins_install.lua
+++ lua/plugins/plugins_install.lua
@@ -20,4 +20,9 @@ return require('packer').startup(function(use)
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 end)
