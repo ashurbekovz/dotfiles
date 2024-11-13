abaae45f62149251973c8fe6cfd394b32130358e
Zamir Ashurbekov <derbent_21@outlook.com>
2024-11-13 21:50:27 +0300

    fix: better nvimtree config

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -1,20 +1,21 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 vim.opt.termguicolors = true
 
-require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
+require('nvim-tree').setup {
+    filters = { dotfiles = false },
+    disable_netrw = true,
+    hijack_cursor = true,
+    sync_root_with_cwd = true,
+    update_focused_file = {
+        enable = true,
+        update_root = false,
     },
     view = {
         width = 30,
+        preserve_window_proportions = true,
     },
     renderer = {
-        group_empty = true,
+        root_folder_label = false,
+        highlight_git = true,
+        indent_markers = { enable = true },
     },
-    filters = {
-        dotfiles = true,
-    },
-})
-
+}
