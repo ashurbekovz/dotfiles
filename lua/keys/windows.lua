4cba23adb49da3b756298b3f5957a191f1cf5ef1
Zamir Ashurbekov <derbent_21@outlook.com>
2024-11-13 21:19:41 +0300

    feat: better hotkeys for navigation

---
+++ lua/keys/windows.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_keymap('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+
