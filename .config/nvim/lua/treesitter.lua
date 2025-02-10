3f12fe7c465577b5026063923c56d35c95877e63
Zamir Ashurbekov <derbent_21@outlook.com>
2025-02-10 19:49:23 +0300

    fix: treesitter for cpp

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "python", "go", "c", "c++" },
+  ensure_installed = { "lua", "python", "go", "c", "cpp" },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
