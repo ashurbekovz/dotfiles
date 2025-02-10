86172726cf57a76e39903ce09578301def52288a
Zamir Ashurbekov <derbent_21@outlook.com>
2025-02-10 19:33:46 +0300

    feat: add treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,12 @@
 return {
     -- Telescope
-    "nvim-telescope/telescope.nvim"
+    "nvim-telescope/telescope.nvim",
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate", -- Обновит парсеры при установке
+        config = function()
+            require("treesitter")
+        end,
+    }
 }
