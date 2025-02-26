4fc9c237458baea41aed19d405041aefdde62c88
Zamir Ashurbekov <derbent_21@outlook.com>
2025-02-26 23:38:28 +0300

    fix: autocomplete mapping

--- .config/nvim/lua/cmp_config.lua
+++ .config/nvim/lua/cmp_config.lua
@@ -2,7 +2,7 @@ local cmp = require("cmp")
 
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-s>"] = cmp.mapping.complete(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping.select_next_item(),
         ["<S-Tab>"] = cmp.mapping.select_prev_item(),
