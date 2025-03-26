410ad1e76881475b6f384135ba451f8b6b87638e
Zamir Ashurbekov <ashurbekovz23@gmail.com>
2025-03-27 02:06:30 +0300

    change model for chat mode codecompanion neovim

--- .config/nvim/lua/configs/codecompanion.lua
+++ .config/nvim/lua/configs/codecompanion.lua
@@ -1,6 +1,6 @@
 require("codecompanion").setup({
     adapters = {
-        my_gemma = function()
+        my_local = function()
             return require("codecompanion.adapters").extend("openai_compatible", {
                 env = {
                     url = "http://127.0.0.1:1234",
@@ -14,14 +14,29 @@ require("codecompanion").setup({
                     },
                 }
             })
+        end,
+        my_online = function()
+            return require("codecompanion.adapters").extend("openai_compatible", {
+                env = {
+                    url = "https://api.vsegpt.ru",
+                    api_key = "cmd: echo $VSEGPT_API_KEY",
+                    chat_url = "/v1/chat/completions",
+                    models_endpoint = "/v1/models",
+                },
+                schema = {
+                    model = {
+                        default = "google/gemini-2.0-flash-001",
+                    },
+                }
+            })
         end
     },
     strategies = {
         chat = {
-            adapter = "my_gemma",
+            adapter = "my_online",
         },
         inline = {
-            adapter = "my_gemma",
+            adapter = "my_local",
         },
     },
 })
