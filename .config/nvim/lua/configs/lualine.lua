3c110debc9dbd3a8067084cbb6c8cbb5b1674dee
Zamir Ashurbekov <ashurbekovz23@gmail.com>
2025-04-13 18:32:26 +0300

    format string neovim

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -1,19 +1,19 @@
 local codecompanion_status = require("configs/lualine_codecompanion").update_status
 
-require('lualine').setup({
+require("lualine").setup({
     options = {
         icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         globalstatus = true,
     },
     sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { { 'filename', path = 1 } },
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "encoding", "filetype" },
+        lualine_y = { "progress" },
         lualine_z = { codecompanion_status }
     },
 })
