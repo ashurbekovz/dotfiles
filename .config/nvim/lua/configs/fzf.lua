8de4c9e11d45561a6ea41ab3ad8d4f0c7d9174a2
Zamir Ashurbekov <ashurbekovz23@gmail.com>
2025-05-08 03:25:58 +0300

    add possiblity to move all fzf-lua results to quickfix

--- .config/nvim/lua/configs/fzf.lua
+++ .config/nvim/lua/configs/fzf.lua
@@ -11,17 +11,21 @@ fzflua.setup({
     actions = {
         files = {
             ['default'] = fzflua.actions.file_edit,
+            ["ctrl-q"] = fzflua.actions.file_sel_to_qf,
         },
         grep = {
             ['default'] = fzflua.actions.file_edit,
+            ["ctrl-q"] = fzflua.actions.file_sel_to_qf,
         },
         buffers = {
+            ['default'] = fzflua.actions.buf_edit,
             ['default'] = fzflua.actions.buf_edit,
         },
     },
     keymap = {
         fzf = {
             ["tab"] = "toggle+up",
+            ["ctrl-a"] = "toggle-all",
         }
     }
 })
