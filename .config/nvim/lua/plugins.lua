5eb55d8c6e29120088491b8300b86fc3860b7c7c
Zamir Ashurbekov <ashurbekovz23@gmail.com>
2025-06-13 11:11:25 +0300

    add leap plugin to neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -149,4 +149,15 @@ return {
             require("configs/mini")
         end
     },
+
+    {
+        "ggandor/leap.nvim",
+        lazy = false,
+        dependencies = {
+            "tpope/vim-repeat",
+        },
+        config = function()
+            require('leap').set_default_mappings()
+        end
+    },
 }
