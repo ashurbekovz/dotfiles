cacbc4d1ac20701786f4bdfbf8823e1680553b53
Zamir Ashurbekov <ashurbekovz23@gmail.com>
2025-10-11 21:17:46 +0300

    fixes

--- .config/nvim/lua/configs/nvimtree.lua
+++ .config/nvim/lua/configs/nvimtree.lua
@@ -1,15 +1,3 @@
-local api = require("nvim-tree.api")
-
-local function edit_or_open()
-  local node = api.tree.get_node_under_cursor()
-  if node.nodes ~= nil then
-    api.node.open.edit()
-  else
-    api.node.open.edit()
-    api.tree.close()
-  end
-end
-
 require("nvim-tree").setup({
     hijack_cursor = true,
     actions = {
@@ -21,13 +9,17 @@ require("nvim-tree").setup({
         sorter = "case_sensitive",
     },
     view = {
-        width = function()
-            return vim.o.columns
-        end,
         float = {
-            enable = false, 
+            enable = true,
+            open_win_config = {
+                relative = "editor",
+                width = 120,
+                height = 33,
+                row = (vim.o.lines - 33) / 2 - 1,
+                col = (vim.o.columns - 120) / 2,
+            },
         },
-    },
+    }, 
     renderer = {
         special_files = {},
         highlight_modified = "name",
@@ -56,8 +48,4 @@ require("nvim-tree").setup({
     filters = {
         dotfiles = false,
     },
-    on_attach = function (bufnr)
-        vim.keymap.set("n", "l", edit_or_open)
-        vim.keymap.set("n", "H", api.tree.collapse_all)
-    end,
 })
